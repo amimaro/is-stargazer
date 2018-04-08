79dc51ef17b6b8ecba729fb1b9447e5879fcf5e0
Amimaro <amir.zahlan@gmail.com>
2018-04-08 13:28:47 -0300

    Add tests :rotating_light:

--- test.js
+++ test.js
@@ -1,8 +1,50 @@
 import test from 'ava'
 import isStargazer from './'
 
-test('is-stargazer does something awesome', t => {
+test('check amimaro at amimaro/is-stargazer', t => {
   t.plan(1)
+  return isStargazer('amimaro/is-stargazer', 'amimaro')
+    .then(
+      res => {
+        console.log('true - ' + res)
+        t.true(res)
+      }
+    )
+    .catch(
+      err => {
+        console.error(err)
+      }
+    )
+})
 
-  t.true(isStargazer())
+test('check amimaro at amimaro/strobe', t => {
+  t.plan(1)
+  return isStargazer('amimaro/strobe', 'amimaro')
+    .then(
+      res => {
+        console.log('false - ' + res)
+        t.false(res)
+      }
+    )
+    .catch(
+      err => {
+        console.error(err)
+      }
+    )
+})
+
+test('check for null', t => {
+  t.plan(1)
+  return isStargazer('', '')
+    .then(
+      res => {
+        console.log('null - ' + res)
+        t.is(res, null)
+      }
+    )
+    .catch(
+      err => {
+        console.error(err)
+      }
+    )
 })
