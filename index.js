8e949ded3e8fc5a60d9489cec6b08f66fcd3c584
Amimaro <amir.zahlan@gmail.com>
2018-04-08 14:23:15 -0300

    Update parameters :art:

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 
 let axios = require('axios')
 
-module.exports = async function isStargazer(repository, username) {
+module.exports = async function isStargazer(params) {
   try {
-    const stargazers = await getStargazers(repository)
-    return hasStargazer(stargazers.data, username)
+    const stargazers = await getStargazers(params.repos, params.token)
+    return hasStargazer(stargazers.data, params.query)
   } catch (err) {
     console.error(`An error occured: ${err}`)
     return null
   }
 }
 
-let getStargazers = async function(repository) {
-  return await axios.get(`https://api.github.com/repos/${repository}/stargazers`)
+let getStargazers = async function(repository, token) {
+  const URI = `https://api.github.com/repos/${repository}/stargazers`
+
+  return await axios.get(URI)
 }
 
 let hasStargazer = function(stargazers, username) {
