fab0bd0a8b80a725c49d09945f31110afecdb670
Amimaro <amir.zahlan@gmail.com>
2018-04-08 12:53:30 -0300

    Check :star:

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 'use strict'
 
-module.exports = function isStargazer (options) {
-  options = options || {}
+let axios = require('axios')
 
-  return true
+module.exports = async function isStargazer(repository, username) {
+  try {
+    const stargazers = await getStargazers(repository)
+    return hasStargazer(stargazers.data, username)
+  } catch (err) {
+    console.error(`An error occured: ${err}`)
+    return null
+  }
+}
+
+let getStargazers = async function(repository) {
+  return await axios.get(`https://api.github.com/repos/${repository}/stargazers`)
+}
+
+let hasStargazer = function(stargazers, username) {
+  for (let stargazer of stargazers) {
+    if (stargazer.login === username)
+      return true
+  }
+  return false
 }
